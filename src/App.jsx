54667ef993629455e95a008117821f45d391fc8e
emrebulak <emrbrnn@gmail.com>
2024-02-29 17:58:10 +0300

    29.02.2024

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+//components
+import Card from './components/Card';
+
+function App() {
+  return (
+    <div className='container'>
+      <Card />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Card.jsx
@@ -0,0 +1,7 @@
+const Card = () => {
+    return (
+        <h1>Deneme</h1>
+    )
+};
+
+export default Card;
