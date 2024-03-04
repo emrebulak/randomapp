5c3d5f8a63d79322f01891608f5f668f9172f838
emrebulak <emrbrnn@gmail.com>
2024-03-04 17:39:04 +0300

    finished

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,46 @@
+import { useState } from 'react';
+
 const Card = () => {
+
+    const [min, setMin] = useState('');
+    const [max, setMax] = useState('');
+    const [randomNumber, setRandomNumber] = useState('');
+
+    const handleGetRandomNumber = () => {
+        if (max > min) {
+            setRandomNumber( getRandomNumber(Number(min), Number(max)));
+        }else{
+            alert('Max value should be greater than Min value');
+        }
+    };
+
+    const getRandomNumber = (min, max) => {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
     return (
-        <h1>Deneme</h1>
+        <div className="card--container">
+            <div className="card--header">
+                {
+                    min === '' || max === '' || randomNumber === '' ? <h3>Enter Min and Max value</h3> : <h4>Random Number : <span>{randomNumber}</span></h4>
+                }
+            </div>
+            <div className="card--body">
+                <div>
+                    <label>Min</label>
+                    <input autoFocus min={0} onChange={(e) => setMin(e.target.value)} type="number" value={min} />
+                </div>
+
+                <div>
+                    <label>Max</label>
+                    <input onChange={(e) => setMax(e.target.value)} type="number" value={max} />
+                </div>
+
+            </div>
+            <div className="card--footer">
+                <button onClick={handleGetRandomNumber} disabled={min === '' || max === ''}>Get random number</button>
+            </div>
+        </div>
     )
 };
 
